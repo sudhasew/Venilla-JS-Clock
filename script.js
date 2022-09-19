a08705cc691c00ac2d9945f97a37e5675479002f
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-09-19 12:38:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,129 @@
+var hour = document.getElementById("hours");
+var mins = document.getElementById("mins");
+var secs = document.getElementById("secs");
+
+var startBtn = document.getElementById("btnStart");
+var stopBtn = document.getElementById("btnStop");
+
+startBtn.addEventListener("click", startClock);
+stopBtn.addEventListener("click", stopClock);
+
+var waitTimer;
+var strTime;
+
+var date = new Date();
+
+function displayTime() {
+  var date = new Date();
+  var currentHour = date.getHours();
+  var currentMinutes = date.getMinutes();
+  var currentSeconds = date.getSeconds();
+
+  var ampm = currentHour >= 12 ? "PM" : "AM";
+
+  currentHour = currentHour % 12;
+  currentHour = currentHour ? currentHour : 12;
+
+  hour.innerHTML = twoDigit(currentHour) + ":";
+  mins.innerHTML = twoDigit(currentMinutes) + ":";
+  secs.innerHTML = twoDigit(currentSeconds) + " " + ampm;
+}
+
+function twoDigit(digit) {
+  if (digit < 10) {
+    digit = "0" + digit;
+  }
+  return digit;
+}
+
+function startClock() {
+  stopClock();
+  waitTimer = setInterval(displayTime, 100);
+}
+
+function stopClock() {
+  clearInterval(waitTimer);
+}
+
+// Toast Container
+
+let toastContainer;
+
+function generateToast({
+  message = `Now Time is :${date}`,
+  background = "#00214d",
+  color = "#fffffe",
+  length = "3000ms",
+}) {
+  toastContainer.insertAdjacentHTML(
+    "beforeend",
+    `<p class="toast" 
+    style="background-color: ${background};
+    color: ${color};
+    animation-duration: ${length}">
+   ${message}
+  </p>`
+  );
+  const toast = toastContainer.lastElementChild;
+  toast.addEventListener("animationend", () => toast.remove());
+}
+
+(function initToast() {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="toast-container"></div>
+  <style>
+  .toast-container {
+    position: fixed;
+    top: -5rem;
+    right: -20rem;
+    display: grid;
+    gap: 20px;
+  }
+  
+  .toast {
+    font-size: 1rem;
+    font-weight: bold;
+    border:1px solid black;
+    line-height: 1;
+    width:300px;
+    text-align:center;
+    padding: 10px 10px;
+    background-color: lightblue;
+    animation: toastIt 3000ms cubic-bezier(0.785, 0.135, 0.15, 0.86) forwards;
+  }
+  
+  @keyframes toastIt {
+    0%,
+    100% {
+      transform: translateY(-150%);
+      opacity: 0;
+    }
+    10%,
+    90% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+    </style>
+    `
+  );
+  toastContainer = document.querySelector(".toast-container");
+})();
+
+startBtn.addEventListener("click", (e) => {
+  generateToast({
+    message: e.currentTarget.dataset.message,
+    background: "hsl(171 100% 46.1%)",
+    color: "hsl(171 100% 13.1%)",
+    length: "5000ms",
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  generateToast({
+    message: "⚠️ You stopped the clock? ⚠️",
+    background: "hsl(350 100% 66.5%)",
+    color: "hsl(350 100% 13.5%)",
+  });
+});
